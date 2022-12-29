2fc3202fe2280b4646fdcfc31716dafa10a11b40
Adel Khalitov <amkhalitov@sberbank.ru>
2022-12-29 23:37:44 +0300

    add request mapping decorator to controller

--- src/main/java/com/example/lal/LalController.java
+++ src/main/java/com/example/lal/LalController.java
@@ -4,18 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("lal")
 public class LalController {
 
     @Autowired
     private LalRepository repository;
 
-    @PostMapping("lal")
+    @PostMapping
     public Lal create(@RequestBody Lal entity) {
         System.out.println(entity.getName());
         return repository.save(entity);
     }
 
-    @GetMapping("lal")
+    @GetMapping
     public Iterable<Lal> get() {
         return repository.findAll();
     }
